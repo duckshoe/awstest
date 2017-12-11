1534c4261948e727d449ca70807a834542a9893d
Gavin MacPherson <gavinmacpherson@me.com>
2017-12-11 15:04:08 -0500

    build commit

--- awstest/api/server.js
+++ awstest/api/server.js
@@ -2,6 +2,8 @@ const PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + './../build'));
+
 app.listen(PORT,() => {
 	console.log("Listening on Port:%s",PORT)
 	console.log("Stop with Ctrl+C");
